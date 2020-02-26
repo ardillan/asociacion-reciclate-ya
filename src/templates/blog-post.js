097d4cb06e73ba9b3365eb5564a2f0ed1b6f61c3
Ardillán <ardillan@me.com>
2020-02-26 20:48:26 +0100

    Añade nuevas clases

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,19 +4,22 @@ import Aside from "../components/aside"
 import { graphql } from "gatsby"
 export default ({ data }) => {
   const post = data.allWordpressPost.edges[0].node
-  console.log(data)
   return (
     <Layout>
-      <div className="columns is-multiline">
+      <div className="columns is-multiline post">
         <Aside />
-        <div className="column is-8">
+        <div className="column is-8-desktop is-12-mobile">
           <img
             src={post.jetpack_featured_media_url}
             style={{ width: `100%`, height: 300 }}
+            alt={post.title}
           />
           <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
 
-          <div dangerouslySetInnerHTML={{ __html: post.content }} />
+          <div
+            className="content"
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
         </div>
       </div>
     </Layout>
