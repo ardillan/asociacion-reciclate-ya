7d773b1cbfc55e86bdc782da81dbe855b21db957
Ardillán <ardillan@me.com>
2020-02-28 18:13:29 +0100

    Actualiza información de pie de página

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,6 +5,14 @@ const footer = () => {
     <footer>
       {new Date().getFullYear()}, Hecho con WordPress y GatsbyJS
       {` `}
+      <p>
+        <small>¿Te gusta esta web?</small>{" "}
+        <small>
+          <a href="http://github.com/ardillan/asociacion-reciclate-ya">
+            Recíclala y crea la tuya propia
+          </a>
+        </small>
+      </p>
     </footer>
   )
 }
