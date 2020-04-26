44bc1db265c7eaae4f61941796768e0636f9ad5e
Ardillán <ardillan@me.com>
2020-04-26 19:48:20 +0200

    Añade menú de hamburguesa

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,10 @@
 import { Link, graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
-import React from "react"
+import React, { useState } from "react"
 
 const Header = ({ data }) => {
+  const [isActive, setisActive] = React.useState(false)
+
   const logo = useStaticQuery(graphql`
     {
       file(name: { eq: "logotipo" }) {
@@ -17,31 +19,44 @@ const Header = ({ data }) => {
 
   return (
     <header id="main-header">
-      <nav>
-        <ul>
-          <li style={{ height: 25, marginTop: -5 }}>
-            <Link to="/">
-              {" "}
-              <Img fixed={logo.file.childImageSharp.fixed} />
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <Link className="navbar-item" to="/">
+            <Img fixed={logo.file.childImageSharp.fixed} />
+          </Link>
+          <a
+            onClick={() => {
+              setisActive(!isActive)
+            }}
+            role="button"
+            className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
+            aria-label="menu"
+            aria-expanded="false"
+          >
+            {" "}
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+        <div className={`navbar-menu ${isActive ? "is-active" : ""}`}>
+          <div className="navbar-start">
+            <Link className="navbar-item" to="/la-asociacion">
+              La asociación
             </Link>
-          </li>
-          <li>
-            <Link to="/">Inicio</Link>
-          </li>
-          <li>
-            <Link to="/la-asociacion">La asociación</Link>
-          </li>
-          <li>
-            <Link to="/salidas">Salidas</Link>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
-        </ul>
+            <Link className="navbar-item" to="/salidas">
+              Salidas
+            </Link>
+            <Link className="navbar-item" to="/blog">
+              Blog
+            </Link>
+          </div>
+        </div>
       </nav>
     </header>
   )
 }
+
 Header.defaultProps = {
   siteTitle: `Asocación Recíclate Ya`,
 }
