a136b7ff884eb5880fd81328bd1dccaf61e45d7a
Ardillán <ardillan@me.com>
2020-05-31 17:19:22 +0200

    Añade entradas fijas en página principal

--- packages/reciclateya-theme/src/Home.js
+++ packages/reciclateya-theme/src/Home.js
@@ -5,14 +5,42 @@ import Link from "./components/Link";
 const Home = ({ state, posts }) => {
   const keys = Object.keys(posts);
 
+  let stickyPosts = [];
+
+  const getStickyPosts = () => {
+    keys.map((id) => {
+      if (posts[id].sticky) {
+        stickyPosts.push(posts[id]);
+      }
+    });
+
+    stickyPosts.sort(function (a, b) {
+      if (a.date < b.date) return 1;
+      if (a.date > b.date) return -1;
+      return 0;
+    });
+  };
+
+  getStickyPosts();
+
   return (
     <>
-      <MainHeader></MainHeader>
+      <MainHeader>
+        <Link href={stickyPosts[0].link} color="initial">
+          <h2>{stickyPosts[0].title.rendered}</h2>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: stickyPosts[0].excerpt.rendered,
+            }}
+          />
+        </Link>
+      </MainHeader>
       <Items>
         {keys.slice(0, 3).map((id) => {
+          console.log();
           return (
             <Link href={posts[id].link} color="initial" key={id}>
-              {posts[id].title.rendered}
+              <h2>{posts[id].title.rendered}</h2>
               <div
                 dangerouslySetInnerHTML={{
                   __html: posts[id].excerpt.rendered,
@@ -41,6 +69,11 @@ const Items = styled.div`
   align-items: space-around;
   padding: 20px;
   column-gap: 20px;
+
+  h2 {
+    font-size: 25px;
+    font-weight: 400;
+  }
 
   div {
     flex-grow: 1;
