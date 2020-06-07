65606b655f041f44a869199c9dcd82500caae11d
Ardillán <ardillan@me.com>
2020-06-07 19:38:26 +0200

    Añade menú móvil

--- packages/reciclateya-theme/src/components/Header.js
+++ packages/reciclateya-theme/src/components/Header.js
@@ -4,8 +4,53 @@ import { styled, css } from "frontity";
 
 import Link from "./Link";
 import waveImage from "../../assets/images/background-wave.svg";
+import menuIcon from "../../assets/images/menu-icon.png";
+import menuIconClose from "../../assets/images/menu-icon-close.png";
 
-const Header = ({ state }) => {
+const Header = ({ state, actions }) => {
+  const Menu = styled.div`
+    display: none;
+    button {
+      background: none;
+      border: none;
+    }
+
+    ul {
+    }
+
+    li {
+      width: 100%;
+      text-align: center;
+      padding: 5px 0;
+      background: #ffec45;
+      list-style-type: none;
+      margin: 15px 0;
+      transform: rotate(1deg);
+      font-size: 22px;
+    }
+
+    /* Mobile */
+    @media screen and (max-width: ${state.theme.screenSizes.mobile}) {
+      display: block;
+    }
+  `;
+
+  const Navigation = styled.nav`
+    text-align: center;
+    display: flex;
+
+    a {
+      font-family: "Barlow";
+      font-weight: 600;
+      padding-right: 20px;
+    }
+
+    /* Mobile */
+    @media screen and (max-width: ${state.theme.screenSizes.mobile}) {
+      display: none;
+    }
+  `;
+
   return (
     <header
       css={css`
@@ -20,6 +65,57 @@ const Header = ({ state }) => {
       min-height:50px;
       `}
     >
+      <Menu>
+        {state.theme.isMenuOpen ? (
+          <>
+            <button onClick={actions.theme.closeMenu}>
+              <img
+                src={menuIconClose}
+                css={css`
+                  width: 25px;
+                  height: 25px;
+                `}
+              />
+            </button>
+            <ul
+              css={css`
+                padding: 0;
+              `}
+            >
+              <li>
+                <Link href="/">Inicio</Link>
+              </li>
+              <li>
+                <Link href="/la-asociacion">La asociación</Link>
+              </li>
+              <li>
+                <Link href="/blog/">Blog</Link>
+              </li>
+              <li>
+                <Link href="/nuestros-amigos/">Nuestros amigos</Link>
+              </li>
+            </ul>
+          </>
+        ) : (
+          <button
+            onClick={actions.theme.openMenu}
+            css={css`
+              background: none;
+              border: none;
+            `}
+          >
+            {" "}
+            <img
+              src={menuIcon}
+              css={css`
+                width: 25px;
+                height: 25px;
+              `}
+            />
+          </button>
+        )}
+      </Menu>
+
       <Navigation
         css={css`
           margin: auto;
@@ -46,14 +142,3 @@ const Header = ({ state }) => {
 };
 
 export default connect(Header);
-
-const Navigation = styled.nav`
-  text-align: center;
-  display: flex;
-
-  a {
-    font-family: "Barlow";
-    font-weight: 600;
-    padding-right: 20px;
-  }
-`;
