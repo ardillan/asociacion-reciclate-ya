c2fe095769ba0bd14544a74fabe575dbb31628b2
Ardillán <ardillan@me.com>
2020-06-07 15:12:23 +0200

    Añade footer

--- packages/reciclateya-theme/src/components/Blog.js
+++ packages/reciclateya-theme/src/components/Blog.js
@@ -4,18 +4,21 @@ import { connect } from "frontity";
 import PostsList from "./PostsList";
 import BreadCrumbs from "../components/BreadCrumbs";
 import Container from "./structure/Container";
-
+import Footer from "./Footer";
 const Blog = ({ state }) => {
   return (
-    <Container>
-      <BreadCrumbs
-        data={[
-          { link: "/", text: "Inicio" },
-          { link: "/blog/", text: "Blog" },
-        ]}
-      />
-      <PostsList />
-    </Container>
+    <>
+      <Container>
+        <BreadCrumbs
+          data={[
+            { link: "/", text: "Inicio" },
+            { link: "/blog/", text: "Blog" },
+          ]}
+        />
+        <PostsList />
+      </Container>
+      <Footer />
+    </>
   );
 };
 
