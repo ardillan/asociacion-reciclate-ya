96e925ee3e176dce5132f6422121c8fdf62f6295
Ardillán <ardillan@me.com>
2020-06-13 23:48:16 +0200

    Añade interactividad en los botones

--- packages/reciclateya-theme/src/components/Link.js
+++ packages/reciclateya-theme/src/components/Link.js
@@ -18,6 +18,12 @@ const Link = ({ state, href, actions, children, type }) => {
           font-size: 18px;
           font-weight: 600;
           text-decoration: none;
+          transition: ${state.theme.effects.transition};
+
+          :hover {
+            transform: scale(115%) rotate(-5deg);
+            background: ${state.theme.colors.darkYellow};
+          }
         `;
 
       case "button-rounded":
@@ -33,12 +39,30 @@ const Link = ({ state, href, actions, children, type }) => {
           font-weight: 600;
           text-decoration: none;
           border-radius: 50px;
+          transition: ${state.theme.effects.transition};
+
+          :hover {
+            transform: scale(105%) rotate(-2deg);
+            background: ${state.theme.colors.darkYellow};
+          }
         `;
       default:
         return styled.a`
           color: ${state.theme.colors.dark};
           text-decoration: none;
           font-family: "Barlow";
+
+          :hover {
+            h2 {
+              text-decoration: underline;
+              text-decoration-color: ${state.theme.colors.yellow};
+              text-decoration-thickness: 3px;
+            }
+
+            p {
+              text-decoration: underline;
+            }
+          }
         `;
     }
   };
