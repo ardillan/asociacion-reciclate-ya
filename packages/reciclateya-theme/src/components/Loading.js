4911ca30616bb287b8ec2cc68fdddd4e1e4ed0ac
Ardillán <ardillan@me.com>
2020-06-26 19:58:56 +0200

    Añade imagen de carga close #14

--- packages/reciclateya-theme/src/components/Loading.js
+++ packages/reciclateya-theme/src/components/Loading.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { css, connect } from "frontity";
+import loaderImage from "../../assets/images/loader.gif";
 import Container from "./structure/Container";
 
 const Loading = () => (
   <Container>
+    <img
+      src={loaderImage}
+      css={css`
+        margin: auto;
+      `}
+    />
     <h1
       css={css`
         text-align: center;
+        font-size: 20px;
       `}
     >
       Cargando contenido
