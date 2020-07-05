1ff4c2007784cefbd22a96050e17ec5f8c03b7ff
Ardillán <ardillan@me.com>
2020-07-05 23:19:45 +0200

    Añade componente Image a imagen principal close #2

--- packages/reciclateya-theme/src/components/Home.js
+++ packages/reciclateya-theme/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect, styled, css } from "frontity";
+import Image from "@frontity/components/image";
 import Link from "./Link";
 import { renderText, formatDate } from "../utils/helpers";
 
@@ -292,7 +293,7 @@ const Home = ({ state, posts }) => {
             position: relative;
           `}
         >
-          <img src={groupImage} />
+          <Image src={groupImage} alt="Foto de grupo de la Asociación" />
         </div>
         <div>
           <h2>Asociación Recíclate YA</h2>
